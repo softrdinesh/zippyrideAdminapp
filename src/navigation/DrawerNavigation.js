9d106e0ac2c0d9cf1ba6588391f70b23c428f652
Kevin Macwan <kmacwan86@gmail.com>
2025-09-05 00:23:08 +0530

    driver list screen added

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -5,35 +5,21 @@ import ProfileScreen from "../Screens/Profile/profileScreen";
 import { VehicleNavigator } from "./VehicleStack";
 import { CustomDrawerContent } from "../uikit/CustomDrawerContent";
 import { TripsNavigator } from "./TripsStack";
+import { DriverNavigator } from "./DriverStack";
 
 const Drawer = createDrawerNavigator();
 
 export const MainDrawer = () => {
   return (
     <Drawer.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
       drawerContent={(props) => <CustomDrawerContent {...props} />}
     >
-      <Drawer.Screen
-        name="VehicleSetup"
-        component={VehicleNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="TripDetails"
-        component={TripsNavigator}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Drawer.Screen
-        name="TrackYourDriver"
-        component={ProfileScreen}
-        options={{
-          headerTitle: "Track your driver",
-        }}
-      />
+      <Drawer.Screen name="VehicleSetup" component={VehicleNavigator} />
+      <Drawer.Screen name="TripDetails" component={TripsNavigator} />
+      <Drawer.Screen name="TrackYourDriver" component={DriverNavigator} />
     </Drawer.Navigator>
   );
 };
